4db18690b3d68d4dc3dfca83391b87987edb0bff
EricLiu27 <eric.liu82794@gmail.com>
2023-07-05 14:35:50 -0400

    Initialize Commit

---
+++ basketball_dictionary.py
@@ -0,0 +1,84 @@
+players = [
+    {
+        "name": "Kevin Durant", 
+        "age":34, 
+        "position": "small forward", 
+        "team": "Brooklyn Nets"
+    },
+    {
+        "name": "Jason Tatum", 
+        "age":24, 
+        "position": "small forward", 
+        "team": "Boston Celtics"
+    },
+    {
+        "name": "Kyrie Irving", 
+        "age":32,
+        "position": "Point Guard", 
+        "team": "Brooklyn Nets"
+    },
+    {
+        "name": "Damian Lillard", 
+        "age":33,
+        "position": "Point Guard", 
+        "team": "Portland Trailblazers"
+    },
+    {
+        "name": "Joel Embiid", 
+        "age":32,
+        "position": "Power Foward", 
+        "team": "Philidelphia 76ers"
+    },
+    {
+        "name": "DeMar DeRozan",
+        "age": 32,
+        "position": "Shooting Guard",
+        "team": "Chicago Bulls"
+    }
+]
+
+# Challenge 1
+
+class Player:
+    def __init__(self, data_dict):
+        self.name = data_dict['name']
+        self.age = data_dict['age']
+        self.position = data_dict['position']
+        self.team = data_dict['team']
+        
+
+# Challenge 2
+
+kevin = {
+    	"name": "Kevin Durant", 
+    	"age":34, 
+    	"position": "small forward", 
+    	"team": "Brooklyn Nets"
+}
+jason = {
+    	"name": "Jason Tatum", 
+    	"age":24, 
+    	"position": "small forward", 
+    	"team": "Boston Celtics"
+}
+kyrie = {
+    	"name": "Kyrie Irving", 
+    	"age":32,
+        "position": "Point Guard", 
+    	"team": "Brooklyn Nets"
+}
+    
+# Create your Player instances here!
+player_jason = jason
+player_kevin = kevin
+player_kyrie = kyrie
+
+
+print(jason["name"], jason["age"], jason["position"], jason["team"])
+# Challenge 3
+
+new_team = []
+for player in players:
+    new_team.append(player)
+
+print(new_team)
